6e5472546af72296496ba4fd2a56a48f7277e4d8
molero3111 <molero3111@gmail.com>
2024-06-08 17:29:50 -0400

    Added authorize service request route

--- routes/api.php
+++ routes/api.php
@@ -11,9 +11,16 @@ Route::post('/login', [AuthenticationController::class, 'login'])->name('login')
 
 Route::post('/logout', [AuthenticationController::class, 'logout'])->name('logout')->middleware('auth:sanctum');
 
+
 Route::group(['middleware' => ['auth:sanctum', 'verify_admin_role:admin']], function () {
     Route::resource('/users', UserController::class);
     Route::resource('/authors', AuthorController::class);
     Route::resource('/books', BookController::class);
     Route::get('/export/authors/books', [ExportController::class, 'exportData']);
+
+    // This request is used by Django api service to authorize its requests bearer tokens
+    // by putting request in middlewares sanctum and verify admin, it performs neccesary checks.
+    Route::post('/authorize-service-request', function(){
+        return response()->json([ 'message' => 'Request authorized'], 201);
+    })->name('service_authorization');
 });
